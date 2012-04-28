819bb1fddf49e11ce662b8cb4d9bef123bef9053
realwsm <realwsm@163.com>
2012-04-28 15:30:02 +0800

    change something

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,13 +1,18 @@
-#include "fillcontent.h"
+#include <iostream>
+#include "fill_content.h"
 
 int main() {
-  fillcontent newconfig;
+  FaultDiagnosis::fillcontent newconfig;
   //std::string path("D:\\temp\\data.txt");
   //newconfig.loadfile(path);
   //newconfig.printcontent();
   //getchar();
-  std::string path("/media/000D418D00029F3E/开题/软件代码、文档/电路板4数据（10.27）/原始数据/NORMAL[1]-D6.2_data_201007210831.out");
+  std::string path("/home/wang/share/tmp/data/NORMAL[1]-D6.2_data_201007210831.out");
   newconfig.loadnewfile(path);
+  std::cout << "print"
+            << std::endl
+            << "-------------------------------------------------"
+            << std::endl;
   newconfig.printnewcontent();
   return 0;
 }
